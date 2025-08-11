4f1422e120ca9269c62afccb9aa283254811c5e3
Vaishnavi <127002291+vishhiy@users.noreply.github.com>
2025-08-11 16:14:33 +0530

    Update printNum.java

--- recursion/printNum.java
+++ recursion/printNum.java
@@ -2,7 +2,7 @@ package recursion;
 
 public class printNum {
     public static void print(int n){
-        if(n==0) return; 
+        if(n==0) return 0; 
         print(n-1);
         System.out.print(n+" ");
     }
